9faee5bb43340e72a68874aa4e748d14b57e2fdf
Mrig26 <mrig.hardwell@gmail.com>
2020-10-10 21:03:30 +0530

    test

---
+++ sfm.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import scapy.all as scapy
+
+def scanit(ip):
+	arp_request = scapy.ARP(pdst=ip)
+	broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+	combined_request = broadcast/arp_request
+	(answered_list, unanswered_list) = scapy.srp(combined_request, timeout=1, verbose=False)
+	print("IP\t\t\tMAC Address\n--------------------------------------------")
+	for element in answered_list:
+		print(element[1].psrc + "\t\t" +element[1].hwsrc)
+
+scanit("192.168.222.2")
